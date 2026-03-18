35bc175231d09d8f513ee9413b31408c34948ad2
xyzdevelopment0 <xyzdevelopment@icloud.com>
2026-03-18 13:25:55 +0300

    feat: remove unused diagnostics

--- src/main/java/com/maximovich/planner/task/diagnostics/dto/EntityCountSnapshot.java
+++
@@ -1,4 +0,0 @@
-package com.maximovich.planner.task.diagnostics.dto;
-
-public record EntityCountSnapshot(long users, long projects, long tasks, long comments, long tags) {
-}
--- src/main/java/com/maximovich/planner/task/diagnostics/dto/ProjectTaskCountResponse.java
+++
@@ -1,4 +0,0 @@
-package com.maximovich.planner.task.diagnostics.dto;
-
-public record ProjectTaskCountResponse(Long projectId, String projectName, int taskCount) {
-}
